01e8feb6c558c547a4a0d91f3cdf3981b8074889
fab777-max <fabien98escoffier@yahoo.com>
2026-09-04 03:51:12 +0200

    Bridge 1.1.7: update build.mjs

--- build.mjs
+++ build.mjs
@@ -3,7 +3,7 @@ import { createHash } from "node:crypto";
 
 const source = await readFile("index.js");
 const actualHash = createHash("sha256").update(source).digest("hex");
-const expectedHash = "114693d3c07e669bb1fec5b741cfd3ff019287340ee7be37a086e196d624b33f";
+const expectedHash = "99bf13510b75c1599c2a309dc0e6b0f6839006a972cbc41fa110fcf94610a936";
 
 if (actualHash !== expectedHash) {
   throw new Error(`index.js integrity check failed: ${actualHash}`);
